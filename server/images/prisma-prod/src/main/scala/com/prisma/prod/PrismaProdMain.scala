cf6441f246a23bb23adc286ac31e9ed3ca0339dd
Dominic Petrick <dompetrick@gmail.com>
2018-04-24 19:44:53 +0200

    Finish renaming

--- server/images/prisma-prod/src/main/scala/com/prisma/prod/PrismaProdMain.scala
+++ server/images/prisma-prod/src/main/scala/com/prisma/prod/PrismaProdMain.scala
@@ -17,9 +17,9 @@ object PrismaProdMain extends App {
 
   dependencies.initialize()(system.dispatcher)
 
-  val port                 = dependencies.config.port.getOrElse(4466)
-  val includeClusterServer = dependencies.config.managmentApiEnabled
-  val servers = includeClusterServer.flatMap(_.toOption(ManagementServer("management", dependencies.config.server2serverSecret))) ++ List(
+  val port              = dependencies.config.port.getOrElse(4466)
+  val includeMgmtServer = dependencies.config.managmentApiEnabled
+  val servers = includeMgmtServer.flatMap(_.toOption(ManagementServer("management", dependencies.config.server2serverSecret))) ++ List(
     WebsocketServer(dependencies),
     ApiServer(dependencies.apiSchemaBuilder),
     SimpleSubscriptionsServer(),
